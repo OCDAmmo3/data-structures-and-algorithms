de8c8c0059a2aa5370b37b83bf890caeda98067f
Jon Struve <ocdammo3@gmail.com>
2019-09-25 14:49:43 -0500

    Finished challenge 8 - merge lists

--- code-challenges/llMerge/ll-merge.js
+++ code-challenges/llMerge/ll-merge.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const LinkedLists = require('../../linkedList/src/linked-list');
+
+let mergeLists = (list1, list2) => {
+  let newList = new LinkedLists();
+  let curr1 = list1.head;
+  let curr2 = list2.head;
+
+  while(curr1 !== null || curr2 !== null) {
+    if(newList.length === 0) {
+      newList.insert(curr1.value);
+      curr1 = curr1.next;
+    }
+    if(curr2 !== null) {
+      newList.append(curr2.value);
+      curr2 = curr2.next;
+    }
+    if(curr1 !== null) {
+      newList.append(curr1.value);
+      curr1 = curr1.next;
+    }
+  };
+  return newList;
+};
+
+module.exports = mergeLists;
--- linkedList/__tests__/linked-list.test.js
+++ linkedList/__tests__/linked-list.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const LinkedLists = require('../src/linked-list');
+const mergeLists = require('../../code-challenges/llMerge/ll-merge');
 
 describe('testing linked list', () => {
   it('should successfully create a linked list', () => {
@@ -127,5 +128,52 @@ describe('testing edits of linked lists', () => {
     expect(() => {
       list.kthFromEnd(3);
     }).toThrow('Error, there is no list here.');
+  });
+
+  it('takes in 2 equal length lists and merges them together, returning one list with the nodes from each alternating between the two', () => {
+    // Arrange
+    let list1 = new LinkedLists();
+    let list2 = new LinkedLists();
+    list1.insert(25);
+    list1.insert(23);
+    list1.insert(21);
+    list1.insert(19);
+    list1.insert(17);
+    list1.insert(15);
+    list2.insert(24);
+    list2.insert(22);
+    list2.insert(20);
+    list2.insert(18);
+    list2.insert(16);
+    list2.insert(14);
+
+    // Act
+    let merged = mergeLists(list1, list2);
+
+    // Assert
+    expect(list1.toString()).toBe('15,17,19,21,23,25');
+    expect(list2.toString()).toBe('14,16,18,20,22,24');
+    expect(merged.toString()).toBe('15,14,17,16,19,18,21,20,23,22,25,24');
+  });
+
+  it('takes in 2 different length lists and merges them together, returning one list with the nodes from each alternating between the two', () => {
+    // Arrange
+    let list1 = new LinkedLists();
+    let list2 = new LinkedLists();
+    list1.insert(25);
+    list1.insert(20);
+    list1.insert(15);
+    list1.insert(5);
+    list1.insert(0);
+    list2.insert(10);
+    list2.insert(1);
+
+    // Act
+    let merged = mergeLists(list1, list2);
+
+    // Assert
+    expect(list1.toString()).toBe('0,5,15,20,25');
+    expect(list2.toString()).toBe('1,10');
+    expect(merged.toString()).toBe('0,1,5,10,15,20,25');
   });
 });
