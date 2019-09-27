db6133a83ae2d135c9baf53f5ffd880d2a2a199e
Jon Struve <ocdammo3@gmail.com>
2019-09-27 14:53:35 -0500

    Added stacks and tests with push, pop, peek, and toString methods

---
+++ stacks-and-queues/stacks/stacks.js
@@ -0,0 +1,53 @@
+'use strict';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.length = 0;
+  }
+
+  toString() {
+    let curr = this.top;
+    let listArray = [];
+    while(curr) {
+      listArray.push(curr.value);
+      curr = curr.next;
+    }
+    return listArray.join(',');
+  }
+
+  push(value) {
+    let newNode = new Node(value);
+    if(this.top) {
+      newNode.next = this.top;
+    }
+    this.top = newNode;
+    this.length++;
+  }
+
+  pop() {
+    if(!this.top) {
+      return;
+    }
+    this.top = this.top.next;
+    this.length--;
+    return this.top;
+  }
+
+  peek() {
+    if(!this.top) {
+      return;
+    }
+    return this.top.value;
+  }
+
+}
+
+module.exports = Stack;
