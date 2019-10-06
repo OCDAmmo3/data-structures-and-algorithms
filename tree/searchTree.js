410fb6cca232ad50cf0bc0b735b1ec95cd250940
Jon Struve <ocdammo3@gmail.com>
2019-10-06 11:07:31 -0500

    Added binary search tests and working add method

--- tree/searchTree.js
+++ tree/searchTree.js
@@ -14,12 +14,22 @@ class BinarySearchTree {
     this.length = 0;
   }
 
-  add(value) {
-
+  add(value, root = this.root) {
+    if(!root) {
+      return this.root = new Node(value);
+    } else if(value < root.value && !root.left) {
+      return root.left = new Node(value);
+    } else if(value > root.value && !root.right) {
+      return root.right = new Node(value);
+    }
+    if(value < root.value) {
+      this.add(value, root.left);
+    } else if(value > root.value) {
+      this.add(value, root.right);
+    }
   }
 
-  contains(value) {
-
+  contains(value, root = this.root) {
   }
 
 }
