e7ddcb91d5907e8038abe50573d5d71a119c3727
Jon Struve <ocdammo3@gmail.com>
2019-10-29 17:07:43 -0500

    Added working hash table

--- hash-tables/hash-table.js
+++ hash-tables/hash-table.js
@@ -1,21 +1,68 @@
 'use strict';
 
+const LinkedList = require('../linkedList/src/linked-list');
+
 class HashTable {
-  constructor(string) {
-    this.string = string;
+  constructor(count) {
+    this.storage = [];
+    this.bucketCount = count || 1024;
   }
 
-  hashMethod() {
-    if(typeof this.string !== 'string') {
-      this.string.toString();
+  add(object) {
+    if(!object || typeof object !== 'object') {
+      return 'Invalid object';
+    }
+    let hashedKey = this.hashMethod(Object.keys(object)[0]);
+    if(!this.storage[hashedKey]) {
+      this.storage[hashedKey] = new LinkedList();
+    }
+    this.storage[hashedKey].insert(object);
+    return this.storage[hashedKey].head;
+  }
+
+  get(key) {
+    if(!key) {
+      return 'Invalid string';
+    }
+    let current;
+    let hashedKey = this.hashMethod(key);
+    if(this.storage[hashedKey]) {
+      current = this.storage[hashedKey].head;
+    } else {
+      current = null;
+    }
+    while(current) {
+      if(current.value[key]) {
+        return current.value;
+      }
+      current = current.next;
+    }
+    return current;
+  }
+
+  contains(key) {
+    if(!key) {
+      return 'Invalid string';
+    }
+    let result = this.get(key);
+    if(!result) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  hashMethod(string) {
+    if(typeof string !== 'string') {
+      string.toString();
     }
     let hashNumber = 0;
     let charNumber;
-    for(let i = 0; i < this.string.length; i++) {
-      charNumber = this.string.charCodeAt(i);
+    for(let i = 0; i < string.length; i++) {
+      charNumber = string.charCodeAt(i);
       hashNumber = hashNumber + charNumber;
     }
-    hashNumber = Math.floor((hashNumber * 599) / 1024);
+    hashNumber = Math.floor((hashNumber * 599) / this.bucketCount);
     return hashNumber;
   }
 }
