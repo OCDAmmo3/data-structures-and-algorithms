2b9f8301140a097bd650e4906a98ee282c355d7e
Jon Struve <ocdammo3@gmail.com>
2019-10-30 15:57:50 -0500

    Wrote test for full left join function

--- hash-tables/left-join/left-join.test.js
+++ hash-tables/left-join/left-join.test.js
@@ -17,3 +17,22 @@ describe('left join tests', () => {
     // Assert
     expect(joined).toEqual([['pet', 'dog'], ['animal', 'creature']]);
   });
+
+  it('can return a hash table with combined right and left tables', () => {
+    // Arrange
+    let hashTableSyn = new HashTable(4096);
+    let hashTableAnt = new HashTable(4096);
+    hashTableSyn.add({cat: 'feline'});
+    hashTableSyn.add({dog: 'bark'});
+    hashTableSyn.add({cow: 'moo'});
+    hashTableAnt.add({cat: 'canine'});
+    hashTableAnt.add({dog: 'meow'});
+    hashTableAnt.add({horse: 'neigh'});
+
+    // Act
+    let joined = leftJoin(hashTableSyn, hashTableAnt);
+
+    // Assert
+    expect(joined).toEqual([['cat', 'feline', 'canine'], ['dog', 'bark', 'meow'], ['cow', 'moo', null]])
+  });
+});
