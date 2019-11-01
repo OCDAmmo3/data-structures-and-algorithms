2ee73fb11eb1d1e2ca4da089ed5c7a01acb43c3a
Jon Struve <ocdammo3@gmail.com>
2019-11-01 12:43:06 -0500

    Added testing shell

---
+++ graphs/graph.test.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('base graph method tests', () => {
+  it('should add a new node to the graph', () => {
+    
+  });
+
+  it('should add a new edge to the graph by adding each node to the others\' neighbor', () => {
+
+  });
+
+  it('should be able to get all the nodes', () => {
+
+  });
+
+  it('should be able to get the neighbors connected by an edge for one node', () => {
+
+  });
+
+  it('can give a size back that is the amount of nodes in the graph', () => {
+
+  });
+});
